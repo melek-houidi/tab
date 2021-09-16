ff243b5c7b4739ee042bd4660a9c8380be424a40
melek houidi <melekhouidi001@gmail.com>
2021-09-16 22:59:15 +0100

    1 commt

---
+++ CrudOperation/CrudOperation/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudOperation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public partial class Employee
+        {
+
+            public int EmployeeId { get; set; }
+            public string EmployeeName { get; set; }
+            public Nullable<decimal> EmployeeSalary { get; set; }
+            public string EmployeeCity { get; set; }
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee model)
+        {
+            _context.Employees.Add(model);
+            _context.SaveChanges();
+            ViewBag.Message = "Data Insert Successfully";
+            return View();
+        }
+
+    }
+}
